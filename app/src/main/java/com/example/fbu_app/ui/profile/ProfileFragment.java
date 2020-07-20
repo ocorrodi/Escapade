654d712a3d4cd5f01bd994a247d9c65c0651f05a
Olivia Corrodi <olc@fb.com>
2020-07-20 14:51:36 -0400

    added basic profile info retrieval

--- app/src/main/java/com/example/fbu_app/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/fbu_app/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,15 +13,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.bumptech.glide.Glide;
 import com.example.fbu_app.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
 
+    public ImageView ivProfileImage;
+    public TextView tvUsername;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
         return root;
     }
 
@@ -30,12 +35,27 @@ public class ProfileFragment extends Fragment {
 
         LinearLayout posts = view.findViewById(R.id.posts);
 
+        ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        tvUsername = view.findViewById(R.id.tvUsername);
+
+        try {
+            Glide.with(getContext()).load(ParseUser.getCurrentUser().getParseFile("profileImage").getFile()).into(ivProfileImage);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        tvUsername.setVisibility(View.VISIBLE);
+
+        tvUsername.setText(ParseUser.getCurrentUser().getUsername());
+
+        //tvUsername.setText("some text");
+
         posts.setOrientation(LinearLayout.HORIZONTAL);
 
         ProfilePostFragment frag = new ProfilePostFragment();
 
         FragmentManager manager = getFragmentManager();
 
-        manager.beginTransaction().replace(R.id.posts, frag, frag.getTag()).commit();
+        //manager.beginTransaction().replace(R.id.posts, frag, frag.getTag()).commit();
     }
 }
