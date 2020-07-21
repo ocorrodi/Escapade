a0904284153f20f452eb2f055ab1f3a1712449e8
Olivia Corrodi <olc@fb.com>
2020-07-21 17:58:20 -0400

    added search bar with autocomplete for home screen

--- app/src/main/java/com/example/fbu_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/fbu_app/ui/home/HomeFragment.java
@@ -1,19 +1,25 @@
 package com.example.fbu_app.ui.home;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -23,20 +29,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.fbu_app.Post;
 import com.example.fbu_app.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
+import static com.example.fbu_app.ui.dashboard.AddFragment.AUTOCOMPLETE_REQUEST_CODE;
 
 public class HomeFragment extends Fragment {
 
     ArrayList<Post> posts;
     PostMapFragment mapFrag;
     PostListFragment listFrag;
+    LatLng latlng;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,15 +72,6 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         final FragmentManager manager = getFragmentManager();
 
-        Button button = view.findViewById(R.id.button4);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //PostDetailDialogFragment newFrag = PostDetailDialogFragment.newInstance();
-                //newFrag.show(manager, "fragment_post_detail");
-            }
-        });
 
         final RelativeLayout layout2 = view.findViewById(R.id.map);
         final RelativeLayout layout1 = view.findViewById(R.id.list);
@@ -87,15 +100,47 @@ public class HomeFragment extends Fragment {
 
         posts = new ArrayList<>();
         //queryPosts();
+
+        // Initialize the SDK
+        Places.initialize(getActivity().getApplicationContext(), getResources().getString(R.string.google_maps_key));
+
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(getContext());
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.ADDRESS_COMPONENTS));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                latlng = place.getLatLng();
+                queryPosts(true);
+            }
+
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
     }
 
-    protected void queryPosts() {
+    protected void queryPosts(final boolean isSearch) {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
 
         query.include(Post.KEY_USER);
-        query.setLimit(10);
+        if (!isSearch) query.setLimit(10);
         query.addDescendingOrder("createdAt");
         query.findInBackground(new FindCallback<Post>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void done(List<Post> posts2, ParseException e) {
                 if (e != null) {
@@ -104,6 +149,9 @@ public class HomeFragment extends Fragment {
                 for (Post post : posts2) {
                     Log.i(TAG, "Post: " + post.getTitle() + " username: " + post.getUser().getUsername());
                 }
+                if (isSearch) {
+                    sortPosts(posts2);
+                }
                 posts.addAll(posts2);
                 listFrag.setPosts(posts2);
                 mapFrag.setPosts(posts2);
@@ -111,7 +159,61 @@ public class HomeFragment extends Fragment {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void sortPosts(List<Post> posts2) {
+        posts2.sort(new Comparator<Post>() {
+            @Override
+            public int compare(Post post, Post t1) {
+                float[] results1 = new float[1];
+                float[] results2 = new float[1];
+                double startLat1 = post.getLocation().getLatitude();
+                double startLong1 = post.getLocation().getLongitude();
+                double startLat2 = t1.getLocation().getLatitude();
+                double startLong2 = t1.getLocation().getLongitude();
+                double endLat = latlng.latitude;
+                double endLong = latlng.longitude;
+                android.location.Location.distanceBetween(startLat1, startLong1, endLat, endLong, results1);
+                android.location.Location.distanceBetween(startLat1, startLong1, endLat, endLong, results2);
+                return Float.compare(results1[0], results2[0]);
+            }
+        });
+        posts2.subList(0, Math.min(posts2.size(), 20));
+    }
+
     public void getPosts() {
-        queryPosts();
+        queryPosts(false);
+    }
+
+    public void onSearchCalled() {
+        // Set the fields to specify which types of place data to return.
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.ADDRESS_COMPONENTS);
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.OVERLAY, fields)
+                .build(getContext());
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId() + ", " + place.getAddress());
+                Toast.makeText(getContext(), "ID: " + place.getId() + "address:" + place.getAddress() + "Name:" + place.getName() + " latlong: " + place.getLatLng(), Toast.LENGTH_LONG).show();
+                String address = place.getAddress();
+                //searchBar.setText(address);
+                latlng = place.getLatLng();
+                // do query with address
+
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Toast.makeText(getContext(), "Error: " + status.getStatusMessage(), Toast.LENGTH_LONG).show();
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
