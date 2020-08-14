1551c6a382b382b43f038ad803ee074be6739aa8
Olivia Corrodi <olc@fb.com>
2020-08-14 15:00:42 -0400

    changes to map view

--- app/src/main/java/com/example/fbu_app/ui/home/PostMapFragment.java
+++ app/src/main/java/com/example/fbu_app/ui/home/PostMapFragment.java
@@ -4,12 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.BounceInterpolator;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fbu_app.Post;
@@ -25,11 +34,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.maps.android.ui.IconGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -191,6 +204,9 @@ public class PostMapFragment extends Fragment implements GoogleMap.OnMapLongClic
         Marker marker = this.googleMap.addMarker(new MarkerOptions().position(latLng)
                 .title(post.getTitle()));
         marker.setTag(post);
+        MarkerOptions marker1 = createMarker(getContext(), latLng, post.getTags().get(0));
+        //marker.setIcon(createMarker(getContext(), latLng, post.getTags().get(0)));
+        //googleMap.addMarker(marker1);
         dropPinEffect(marker);
     }
 
@@ -198,5 +214,57 @@ public class PostMapFragment extends Fragment implements GoogleMap.OnMapLongClic
     @Override
     public void onMapLongClick(LatLng latLng) {
         getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new AddFragment(latLng)).addToBackStack(null).commit();
+    }
+
+    public static Bitmap getBitmapFromView(View view) {
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        Drawable bgDrawable =view.getBackground();
+        if (bgDrawable!=null)
+            bgDrawable.draw(canvas);
+        else
+            canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
+        return returnedBitmap;
+    }
+
+    private void arrayToChips(List<String> tags, ChipGroup tagChipGroup) {
+        tagChipGroup.removeAllViews();
+        Chip chip =
+                (Chip) getLayoutInflater().inflate(R.layout.item_chip, tagChipGroup, false);
+        for (int i = 0; i < tags.size(); i++) {
+            chip.setText(tags.get(i));
+            tagChipGroup.addView(chip, tagChipGroup.getChildCount() - 1);
+        }
+    }
+
+    public static MarkerOptions createMarker(Context context, LatLng point, String bedroomCount) {
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(point);
+        View markerView = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.marker_item, null);
+        markerView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        int px = getHeight(bedroomCount)*2;
+        markerView.layout(0, 0, px, 50);
+        markerView.buildDrawingCache();
+        TextView bedNumberTextView = (TextView)markerView.findViewById(R.id.textView2);
+        Bitmap mDotMarkerBitmap = Bitmap.createBitmap(px, 50, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(mDotMarkerBitmap);
+        bedNumberTextView.setText(bedroomCount);
+        markerView.draw(canvas);
+        marker.icon(BitmapDescriptorFactory.fromBitmap(mDotMarkerBitmap));
+        return marker;
+    }
+
+    public static int getHeight(String str) {
+
+        Paint paint = new Paint();
+        paint.setTextSize(20);
+        //Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "Helvetica.ttf");
+        paint.setTypeface(Typeface.create("roboto",Typeface.BOLD));
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        Rect result = new Rect();
+        paint.getTextBounds(str, 0, str.length(), result);
+        return result.width();
     }
 }
